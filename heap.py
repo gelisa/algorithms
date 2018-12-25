cd908af9552db52053f7a4f4e53852f7326612e6
Eliza Guseva <liza.guseva@kolabnow.ch>
2018-12-25 13:31:02 -0500

    modified:   heap.py

--- heap.py
+++ heap.py
@@ -1,53 +1,50 @@
+# binary heap
 
 
+class Bin_Heap:
+    def __init__(self, alist=[]):
+        if not alist:
+            self.items = [0]
+            self.size = 0
+        else:
+            self.items = self.init_from_list(alist)
+            self.size = len(self.items)
 
+    def init_from_list(self, iter: list):
+        idx = len(iter) // 2
+        items = [0] + iter
+        while idx > 1:
+            self.perc_up(idx)
+            idx -= 1
+        return items
 
-class BinHeap:
-    def __init__(self):
-        self.heapList = [0]
-        self.currentSize = 0
+    def perc_up(self, cidx: int):
+        while self.items[cidx] < self.items[cidx // 2]:
+            self.items[cidx], self.items[cidx // 2] = self.items[cidx // 2], self.items[cidx]
+            cidx = cidx // 2
 
-    def percUp(self,i):
-        while i // 2 > 0:
-            if self.heapList[i] < self.heapList[i // 2]:
-                self.heapList[i], self.heapList[i // 2] = self.heapList[i // 2], self.heapList[i]
-                i = i // 2
-            else:
-                break
+    def perc_down(self, cidx: int):
+        midx = self.min_child(cidx)
+        while self.items[cidx] > self.items[midx]:
+            self.items[cidx], self.items[midx] = self.items[midx], self.items[cidx]
+            cidx = midx
 
-    def insert(self,k):
-        self.heapList.append(k)
-        self.currentSize = self.currentSize + 1
-        self.percUp(self.currentSize)
+    def insert(self, item):
+        self.items.append(item)
+        self.size += 1
+        self.perc_up(self.size - 1)
 
-    def minChild(self,i):
-        if self.heapList[2*i] > self.heapList[2*i+1]:
+    def min(self):
+        minimum = self.items[1]
+        self.size -= 1
+        self.perc_down(1)
+        return minimum
+
+    def min_child(self, i):
+        if self.items[2 * i] > self.items[2 * i + 1]:
             return 2 * i
         else:
-            return 2 * i + 1    
-        
-    def percDown(self,i):
-        while i * 2 <= self.currentSize:
-            self.heapList[i], self.heapList[self.minChild(i)] =  self.heapList[self.minChild(i)], self.heapList[i]
+            return 2 * i + 1
 
-    def delMin(self):
-        retval = self.heapList[1]
-        self.heapList[1] = self.heapList[self.currentSize]
-        self.currentSize = self.currentSize - 1
-        self.heapList.pop()
-        self.percDown(1)
-        return retval
 
-    def buildHeap(self,alist):
-        i = len(alist) // 2
-        self.currentSize = len(alist)
-        self.heapList = [0] + alist[:]
-        while (i > 0):
-            self.percDown(i)
-            i = i - 1
-        
-
-bh = BinHeap()
-for item in [1,2,3,8,4,7,0,5]:
-    bh.insert(item)
-    print(bh.heapList)
+bh = Bin_Heap()
