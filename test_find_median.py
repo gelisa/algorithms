f0a008962194aabcfc824f89ddf1bd452c3704eb
Eliza Guseva <liza.guseva@kolabnow.ch>
2018-12-26 20:11:21 -0500

    style fixes

--- test_find_median.py
+++ test_find_median.py
@@ -4,34 +4,28 @@ from find_median import find_median
 
 class Test(unittest.TestCase):
     def test_0(self):
-        l = []
-        self.assertIsNone(find_median(l))
+        self.assertIsNone(find_median([]))
 
     def test_1(self):
-        l = [0]
-        self.assertEqual(l[0], find_median(l))
+        self.assertEqual(0, find_median([0]))
 
     def test_2(self):
-        l = [0, 1]
-        self.assertEqual(1, find_median(l))
+        self.assertEqual(1, find_median([0, 1]))
 
     def test_3(self):
-        l = [0, 1, 2]
-        self.assertEqual(1, find_median(l))
+        self.assertEqual(1, find_median([0, 1, 2]))
 
     def test_4(self):
-        l = [0, 1, 2, 3]
-        self.assertEqual(2, find_median(l))
+        self.assertEqual(2, find_median([0, 1, 2, 3]))
 
     def test_some_equal(self):
-        l = [0, 1, 1, 2, 3]
-        self.assertEqual(1, find_median(l))
+        self.assertEqual(1, find_median([0, 1, 1, 2, 3]))
 
     def test_inverse(self):
         self.assertEqual(3, find_median([5, 4, 3, 2, 1]))
 
-    def test_inverse(self):
-        self.assertEqual(3, find_median([5, 4, 3, 2, 1]))
+    def test_unsorted(self):
+        self.assertEqual(3, find_median([1, 3, 4, 5, 2]))
 
 
 if __name__ == '__main__':
