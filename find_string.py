ac0bb688df13550172b916daadb4fb434e0a9df7
Eliza Guseva <liza.guseva@kolabnow.ch>
2018-12-27 19:43:40 -0500

    find string brute force and tests

--- find_string.py
+++ find_string.py
@@ -2,10 +2,16 @@
 
 
 def find_string_bf(string, query):
+    if query == '':
+        return []
+
     locations = []
+    ls = len(string)
     for (i, s) in enumerate(string):
         for (j, qs) in enumerate(query):
-            if qs != string[i+j]:
+            if (i+j < ls) and (qs != string[i+j]):
+                break
+            elif i+j >= ls:
                 break
         else:
             locations.append(i)
--- test_find_string.py
+++ test_find_string.py
@@ -1,1 +1,38 @@
 import unittest
+import ddt
+import find_string as fs
+
+
+@ddt.ddt
+class Test(unittest.TestCase):
+    @ddt.data(fs.find_string_bf)
+    def test_both_empty(self, func):
+        self.assertEqual(func('', ''), [])
+
+    @ddt.data(fs.find_string_bf)
+    def test_string_empty(self, func):
+        self.assertEqual(func('', 'a'), [])
+
+    @ddt.data(fs.find_string_bf)
+    def test_query_empty(self, func):
+        self.assertEqual(func('a', ''), [])
+
+    @ddt.data(fs.find_string_bf)
+    def test_equal(self, func):
+        self.assertEqual(func('a', 'a'), [0])
+
+    @ddt.data(fs.find_string_bf)
+    def test_repeat_1(self, func):
+        self.assertEqual(func('aaa', 'a'), [0, 1, 2])
+
+    @ddt.data(fs.find_string_bf)
+    def test_repeat_2(self, func):
+        self.assertEqual(func('ababab', 'ab'), [0, 2, 4])
+
+    @ddt.data(fs.find_string_bf)
+    def test_overlap(self, func):
+        self.assertEqual(func('abababcabababcc', 'abab'), [0, 2, 7, 9])
+
+    @ddt.data(fs.find_string_bf)
+    def test_end(self, func):
+        self.assertEqual(func('ababccab', 'abab'), [0])
